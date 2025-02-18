df1376343ad66d614e174c763fc6142c8da43f03
Alper Özdemir <alozdemir23@gmail.com>
2025-02-18 19:47:18 +0300

    Exchange typelardan Direct type yapıldı

--- RabbitMQ.Publisher/publisher.cs
+++ RabbitMQ.Publisher/publisher.cs
@@ -3,54 +3,95 @@ using System.Text;
 
 class publisher
 {
+    public enum LogNames
+    {
+        Critical =1,
+        Error=2,
+        Warning=3,
+        Info =4
+    }
     static void Main(string[] args)
     {
-        //*************** 1 VE 2. DERSLERDE YAZILAN KODLAR***************//
-        //var factory = new ConnectionFactory();
-        //factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
-
-        //using var connection = factory.CreateConnection();
-        //var channel = connection.CreateModel();
-
-        ////Durable değerini true yaparsam rabbitmq de fiziksel olarak kaydedilir.
-        ////Ama false yaparsan memoryde geçici yer edinir ve açıp kapadığımda silinir.
-        //channel.QueueDeclare("Hello-Queue",true,false,false);
-        //// rabbit mq mesajlar byte dizisi olarak gider bu yüzden her şey gönderilebilir.
-
-        //Enumerable.Range(1, 50).ToList().ForEach(x =>
-        //{//Burası 50 tane mesaj göndermeye yarıyor
-        //    string message = $"Message {x}";
-        //    var messageBody = Encoding.UTF8.GetBytes(message);
-        //    channel.BasicPublish(string.Empty, "Hello-Queue", null, messageBody);
-
-        //    Console.WriteLine($"Mesaj gönderildi : {message}");
-        //});
-        ////string message = "hello worl";
-        ////var messageBody = Encoding.UTF8.GetBytes(message);
-        ////channel.BasicPublish(string.Empty, "Hello-Queue", null, messageBody);
-
-        ////Console.WriteLine("Mesaj gönderildi");
-        //Console.ReadLine();
-
 
         //****************** 3. Ders Exchange Types ***************//
-        //1. Fanout 
+        //2.Direct
         var factory = new ConnectionFactory();
         factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
 
         using var connection = factory.CreateConnection();
         var channel = connection.CreateModel();
 
-        channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);
+        channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+        Enum.GetNames(typeof(LogNames)).ToList().ForEach(x=> {
+
+            var queueName = $"direct-queuq-{x}";
+            var rootKey = $"route-{x}";
+            channel.QueueDeclare(queueName,true,false,false);
+            channel.QueueBind(queueName, "logs-direct", rootKey,null);
+        });
 
         Enumerable.Range(1, 50).ToList().ForEach(x =>
         {
-            string message = $"log {x}";
-            var messageBody = Encoding.UTF8.GetBytes(message);
-            channel.BasicPublish("logs-fanout","", null, messageBody);
+            LogNames log = (LogNames)new Random().Next(1,5);
+            string message = $"log-type :{log}";
 
-            Console.WriteLine($"Mesaj gönderildi : {message}");
+            var rootKey = $"route-{log}";
+
+            var messageBody = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("logs-direct", rootKey, null, messageBody);
+
+            Console.WriteLine($"log gönderildi : {message}");
         });
         Console.ReadLine();
     }
+
 }
+
+//*************** 1 VE 2. DERSLERDE YAZILAN KODLAR***************//
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
+
+//using var connection = factory.CreateConnection();
+//var channel = connection.CreateModel();
+
+////Durable değerini true yaparsam rabbitmq de fiziksel olarak kaydedilir.
+////Ama false yaparsan memoryde geçici yer edinir ve açıp kapadığımda silinir.
+//channel.QueueDeclare("Hello-Queue",true,false,false);
+//// rabbit mq mesajlar byte dizisi olarak gider bu yüzden her şey gönderilebilir.
+
+//Enumerable.Range(1, 50).ToList().ForEach(x =>
+//{//Burası 50 tane mesaj göndermeye yarıyor
+//    string message = $"Message {x}";
+//    var messageBody = Encoding.UTF8.GetBytes(message);
+//    channel.BasicPublish(string.Empty, "Hello-Queue", null, messageBody);
+
+//    Console.WriteLine($"Mesaj gönderildi : {message}");
+//});
+////string message = "hello worl";
+////var messageBody = Encoding.UTF8.GetBytes(message);
+////channel.BasicPublish(string.Empty, "Hello-Queue", null, messageBody);
+
+////Console.WriteLine("Mesaj gönderildi");
+//Console.ReadLine();
+
+
+//****************** 3. Ders Exchange Types ***************//
+//1. Fanout 
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
+
+//using var connection = factory.CreateConnection();
+//var channel = connection.CreateModel();
+
+//channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);
+
+//Enumerable.Range(1, 50).ToList().ForEach(x =>
+//{
+//    string message = $"log {x}";
+//    var messageBody = Encoding.UTF8.GetBytes(message);
+//    channel.BasicPublish("logs-fanout","", null, messageBody);
+
+//    Console.WriteLine($"Mesaj gönderildi : {message}");
+//});
+//Console.ReadLine();
--- RabbitMQ.Subscriber/subscriber.cs
+++ RabbitMQ.Subscriber/subscriber.cs
@@ -6,57 +6,37 @@ class subscriber
 {
     static void Main(string[] args)
     {
-        //*************** 1 VE 2. DERSLERDE YAZILAN KODLAR***************//
-        //var factory = new ConnectionFactory();
-        //factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
 
-        //using var connection = factory.CreateConnection();
-        //var channel = connection.CreateModel();
-
-        ////Durable değerini true yaparsam rabbitmq de fiziksel olarak kaydedilir.
-        ////Ama false yaparsan memoryde geçici yer edinir ve açıp kapadığımda silinir.
-        ////channel.QueueDeclare("Hello-Queue", true, false, true);
-        //channel.BasicQos(0, 1, false);
-        ////burda birden fazla subscriber çalıştırmak için windows komut isteminden
-        ////subscriber dosyasının yolunu girip ordan her iki ekrandanda dotnet run
-        //// yapılırsa iki farklı subscriber ve channel olur.
-        //var subscriber = new EventingBasicConsumer(channel);
-        //channel.BasicConsume("Hello-Queue",false,subscriber);
-        //subscriber.Received += (object? sender, BasicDeliverEventArgs e)=> {
-        //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
-        //    Thread.Sleep(1500);
-        //    Console.WriteLine(message);
-        //    channel.BasicAck(e.DeliveryTag, false);
-        //};
-
-        //Console.ReadLine();
-        
         //****************** 3. Ders Exchange Types ***************//
-        // 1.Fanout
+        // 2.direct
         var factory = new ConnectionFactory();
         factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
 
         using var connection = factory.CreateConnection();
         var channel = connection.CreateModel();
 
-        var randomQueueName = channel.QueueDeclare().QueueName;
-        
-        //var randomQueueName = "log-database-save-queue"; // burdaki yaptığım değişiklik queuenin kalısı olmasını sağladı.
-        //channel.QueueDeclare(randomQueueName, true, false, false);
-        // bu durumda queue lar kalıcı olmuyorlar bağlantıları koptuklarında gidiyolar.
-        //eğer kaydedilmesi isteniyorsa yukarıdaki yazılabilir
-        channel.QueueBind(randomQueueName, "logs-fanout", "", null);
         channel.BasicQos(0, 1, false);
 
         var subscriber = new EventingBasicConsumer(channel);
-        channel.BasicConsume(randomQueueName, false, subscriber);
+
+
+        //string arg = args[0];
+        //var queueName = $"direct-queuq-{arg}";
+        //linux üzerinden çalıştırılırsa birden fazla pencerede birden fazla
+        //direct type ı görebilmek için kullanıllır
+
+        var queueName = "direct-queuq-Error";
+        channel.BasicConsume(queueName, false, subscriber);
 
         Console.WriteLine("loglar dinleniyor");
-        
-        subscriber.Received += (object? sender, BasicDeliverEventArgs e) => {
+
+        subscriber.Received += (object? sender, BasicDeliverEventArgs e) =>
+        {
             var message = Encoding.UTF8.GetString(e.Body.ToArray());
             Thread.Sleep(1000);
             Console.WriteLine(message);
+
+            File.AppendAllText("log-critical.txt", message + "\n");
             channel.BasicAck(e.DeliveryTag, false);
         };
 
@@ -65,3 +45,61 @@ class subscriber
     }
 
 }
+
+//*************** 1 VE 2. DERSLERDE YAZILAN KODLAR***************//
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
+
+//using var connection = factory.CreateConnection();
+//var channel = connection.CreateModel();
+
+////Durable değerini true yaparsam rabbitmq de fiziksel olarak kaydedilir.
+////Ama false yaparsan memoryde geçici yer edinir ve açıp kapadığımda silinir.
+////channel.QueueDeclare("Hello-Queue", true, false, true);
+//channel.BasicQos(0, 1, false);
+////burda birden fazla subscriber çalıştırmak için windows komut isteminden
+////subscriber dosyasının yolunu girip ordan her iki ekrandanda dotnet run
+//// yapılırsa iki farklı subscriber ve channel olur.
+//var subscriber = new EventingBasicConsumer(channel);
+//channel.BasicConsume("Hello-Queue",false,subscriber);
+//subscriber.Received += (object? sender, BasicDeliverEventArgs e)=> {
+//    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+//    Thread.Sleep(1500);
+//    Console.WriteLine(message);
+//    channel.BasicAck(e.DeliveryTag, false);
+//};
+
+//Console.ReadLine();
+
+//****************** 3. Ders Exchange Types ***************//
+// 1.Fanout
+//var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqps://klihfdct:Tp5GHLQjqsmXgG_1544BvHhTgdnKTkNs@kebnekaise.lmq.cloudamqp.com/klihfdct");
+
+//using var connection = factory.CreateConnection();
+//var channel = connection.CreateModel();
+
+
+
+//var randomQueueName = channel.QueueDeclare().QueueName; // burası random queue ismi oluşturmaya yarıyor.
+
+////var randomQueueName = "log-database-save-queue"; // burdaki yaptığım değişiklik queuenin kalısı olmasını sağladı.
+////channel.QueueDeclare(randomQueueName, true, false, false);
+//// bu durumda queue lar kalıcı olmuyorlar bağlantıları koptuklarında gidiyolar.
+////eğer kaydedilmesi isteniyorsa yukarıdaki yazılabilir
+//channel.QueueBind(randomQueueName, "logs-fanout", "", null);
+//channel.BasicQos(0, 1, false);
+
+//var subscriber = new EventingBasicConsumer(channel);
+//channel.BasicConsume(randomQueueName, false, subscriber);
+
+//Console.WriteLine("loglar dinleniyor");
+
+//subscriber.Received += (object? sender, BasicDeliverEventArgs e) => {
+//    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+//    Thread.Sleep(1000);
+//    Console.WriteLine(message);
+//    channel.BasicAck(e.DeliveryTag, false);
+//};
+
+//Console.ReadLine();
